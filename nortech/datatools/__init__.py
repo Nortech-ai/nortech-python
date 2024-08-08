fac56d6480949f7ab567934fdf3a6d4133f430b9
Carlos Marques <carlos@nortech.ai>
2024-08-08 15:32:51 +0200

    feat: download data (#3)
    * feat: add new download_data function that only supports cold storage

--- nortech/datatools/__init__.py
+++ nortech/datatools/__init__.py
@@ -1,5 +1,15 @@
+from nortech.datatools.gateways.customer_api import CustomerAPI, CustomerAPISettings
+from nortech.datatools.handlers.download import download_data
 from nortech.datatools.handlers.pandas import get_df
 from nortech.datatools.handlers.polars import get_lazy_polars_df, get_polars_df
 from nortech.datatools.values.signals import TimeWindow
 
-__all__ = ["TimeWindow", "get_lazy_polars_df", "get_polars_df", "get_df"]
+__all__ = [
+    "TimeWindow",
+    "get_lazy_polars_df",
+    "get_polars_df",
+    "get_df",
+    "CustomerAPI",
+    "CustomerAPISettings",
+    "download_data",
+]
