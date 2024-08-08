27f36f4fe075c82ada26ed8e3b734ae2009a2f1f
Carlos-Marques <carlosmarques.personal@gmail.com>
2024-08-08 16:28:51 +0200

    fix formatting

--- flake.nix
+++ flake.nix
@@ -9,43 +9,44 @@
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsForAllSystems = forAllSystems (system: import nixpkgs { 
+      pkgsForAllSystems = forAllSystems (system: import nixpkgs {
         inherit system;
-        config = { allowUnfree = true; }; 
+        config = { allowUnfree = true; };
       });
 
-      makeDevShell = system: let
-        inherit pkgsForAllSystems;
-        pkgs = pkgsForAllSystems.${system};
-        pythonPkg = pkgs.python310;
-        pythonEnv = pythonPkg.buildEnv.override {
-          extraLibs = [ pythonPkg.pkgs.pip pythonPkg.pkgs.virtualenv ];
+      makeDevShell = system:
+        let
+          inherit pkgsForAllSystems;
+          pkgs = pkgsForAllSystems.${system};
+          pythonPkg = pkgs.python39;
+          pythonEnv = pythonPkg.buildEnv.override {
+            extraLibs = [ pythonPkg.pkgs.pip pythonPkg.pkgs.virtualenv ];
+          };
+
+          isDarwin = pkgs.stdenv.hostPlatform.isDarwin;
+          darwinPackages = if isDarwin then [ ] else [ ];
+        in
+        pkgs.mkShell {
+          buildInputs = [
+            pythonEnv
+            pkgs.git
+          ] ++ darwinPackages;
+
+          shellHook = ''
+            # Create a virtual environment
+            virtualenv .venv
+
+            # Activate the virtual environment
+            source .venv/bin/activate
+
+            # Install poetry inside the virtual environment
+            pip install poetry==1.4.2
+          '';
         };
-
-        isDarwin = pkgs.stdenv.hostPlatform.isDarwin;
-        darwinPackages = if isDarwin then [ ] else [ ];
-      in
-      pkgs.mkShell {
-        buildInputs = [
-          pythonEnv
-          pkgs.git
-        ] ++ darwinPackages;
-
-        shellHook = ''
-          # Create a virtual environment
-          virtualenv .venv
-
-          # Activate the virtual environment
-          source .venv/bin/activate
-
-          # Install poetry inside the virtual environment
-          pip install poetry==1.4.2 
-        '';
-      };
     in
     {
       devShells = forAllSystems (system: {
-          default = makeDevShell system;
+        default = makeDevShell system;
       });
     };
 }
