d109978d4e4d5334f35c8f7b2c296e9a3451f0f7
Carlos-Marques <carlosmarques.personal@gmail.com>
2024-08-08 15:29:11 +0200

    fix formatting

--- nortech/datatools/__init__.py
+++ nortech/datatools/__init__.py
@@ -4,4 +4,12 @@ from nortech.datatools.handlers.pandas import get_df
 from nortech.datatools.handlers.polars import get_lazy_polars_df, get_polars_df
 from nortech.datatools.values.signals import TimeWindow
 
-__all__ = ["TimeWindow", "get_lazy_polars_df", "get_polars_df", "get_df", "CustomerAPI", "CustomerAPISettings", "download_data"]
+__all__ = [
+    "TimeWindow",
+    "get_lazy_polars_df",
+    "get_polars_df",
+    "get_df",
+    "CustomerAPI",
+    "CustomerAPISettings",
+    "download_data",
+]
