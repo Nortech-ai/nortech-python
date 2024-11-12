565be7f0828ebd7da666552d0d2dff4edc74be17
Carlos Marques <carlosmarques.personal@gmail.com>
2024-11-12 15:25:42 +0000

    Change mermaid node hashing to use sha256

--- nortech/derivers/services/visualize.py
+++ nortech/derivers/services/visualize.py
@@ -1,3 +1,5 @@
+from hashlib import sha256
+
 from nortech.derivers.values.schema import DeriverSchemaDAG
 
 
@@ -8,24 +10,26 @@ def create_deriver_schema_subgraph(deriver_schema_dag: DeriverSchemaDAG):
 
     for deriver_input in deriver_schema_dag.inputs:
         if deriver_input.name != "timestamp":
+            node_id = sha256(deriver_schema_dag.name.encode()).hexdigest()[:8]
             if deriver_input.physical_quantity:
                 mermaid += f"""
-                    {deriver_schema_dag.name.__hash__()}_{deriver_input.name}["{deriver_input.name}<br/>[{deriver_input.physical_quantity.si_unit_symbol.replace(" ", "")}]"] --> transform_stream_{deriver_schema_dag.name.__hash__()}["transform_stream"]
+                    {node_id}_{deriver_input.name}["{deriver_input.name}<br/>[{deriver_input.physical_quantity.si_unit_symbol.replace(" ", "")}]"] --> transform_stream_{node_id}["transform_stream"]
                 """
             else:
                 mermaid += f"""
-                    {deriver_schema_dag.name.__hash__()}_{deriver_input.name}["{deriver_input.name}"] --> transform_stream_{deriver_schema_dag.name.__hash__()}["transform_stream"]
+                    {node_id}_{deriver_input.name}["{deriver_input.name}"] --> transform_stream_{node_id}["transform_stream"]
                 """
 
     for deriver_output in deriver_schema_dag.outputs:
         if deriver_output.name != "timestamp":
+            node_id = sha256(deriver_schema_dag.name.encode()).hexdigest()[:8]
             if deriver_output.physical_quantity:
                 mermaid += f"""
-                    transform_stream_{deriver_schema_dag.name.__hash__()} --> {deriver_schema_dag.name.__hash__()}_{deriver_output.name}["{deriver_output.name}<br/>[{deriver_output.physical_quantity.si_unit_symbol.replace(" ", "")}]"]
+                    transform_stream_{node_id} --> {node_id}_{deriver_output.name}["{deriver_output.name}<br/>[{deriver_output.physical_quantity.si_unit_symbol.replace(" ", "")}]"]
                 """
             else:
                 mermaid += f"""
-                    transform_stream_{deriver_schema_dag.name.__hash__()} --> {deriver_schema_dag.name.__hash__()}_{deriver_output.name}["{deriver_output.name}"]
+                    transform_stream_{node_id} --> {node_id}_{deriver_output.name}["{deriver_output.name}"]
                 """
 
     mermaid += """
@@ -44,8 +48,10 @@ def create_deriver_schema_dag_mermaid(mermaid: str, deriver_schema_dag: DeriverS
                     deriver_schema_dag=suggested_input.deriver_schema_dag,
                 )
 
+                source_id = sha256(suggested_input.deriver_schema_dag.name.encode()).hexdigest()[:8]
+                target_id = sha256(deriver_schema_dag.name.encode()).hexdigest()[:8]
                 mermaid += f"""
-                    {suggested_input.deriver_schema_dag.name.__hash__()}_{suggested_input.name} -.->|suggestedInput| {deriver_schema_dag.name.__hash__()}_{deriver_input.name}
+                    {source_id}_{suggested_input.name} -.->|suggestedInput| {target_id}_{deriver_input.name}
                 """
 
     mermaid += create_deriver_schema_subgraph(deriver_schema_dag=deriver_schema_dag)
